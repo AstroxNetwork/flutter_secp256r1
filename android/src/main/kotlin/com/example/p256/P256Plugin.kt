8e8c6f32741a9356680199d737df3ac1d382f0c6
Alex Li <github@alexv525.com>
2023-01-10 18:43:38 +0800

    ✨ Generate public key on Android

--- android/src/main/kotlin/com/example/p256/P256Plugin.kt
+++ android/src/main/kotlin/com/example/p256/P256Plugin.kt
@@ -1,35 +1,72 @@
 package com.example.p256
 
-import androidx.annotation.NonNull
-
+import android.content.Context
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.interfaces.ECPublicKey
+import java.security.spec.ECGenParameterSpec
 
 /** P256Plugin */
-class P256Plugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class P256Plugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private var applicationContext: Context? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "p256")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "p256")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPublicKey") {
+            val alias = call.argument<String>("tag")!!
+            val ks: KeyStore = KeyStore.getInstance("AndroidKeyStore").apply {
+                load(null)
+            }
+            if (ks.containsAlias(alias)) {
+                val entry: KeyStore.Entry = ks.getEntry(alias, null)
+                if (entry !is KeyStore.PrivateKeyEntry) {
+                    result.error(
+                        "Wrong type",
+                        "Not an instance of a PrivateKeyEntry",
+                        entry.javaClass.canonicalName
+                    )
+                    return
+                }
+                result.success(entry.certificate.publicKey.encoded)
+                return
+            }
+            val kpg: KeyPairGenerator = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore"
+            )
+            val parameterSpec = KeyGenParameterSpec.Builder(
+                alias, KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+            ).run {
+                setAlgorithmParameterSpec(ECGenParameterSpec("secp256r1"))
+                build()
+            }
+            kpg.initialize(parameterSpec)
+            val keyPair = kpg.generateKeyPair()
+            val publicKey = keyPair.public as ECPublicKey
+            result.success(publicKey.encoded)
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        applicationContext = null
+    }
 }
